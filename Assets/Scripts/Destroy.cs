3e97540864faf3e07ac9f0143773891700f5582b
Zuhairi Rahmad <zuhairi.bit@gmail.com>
2019-02-21 00:04:19 +0900

    push for poof animation

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -6,6 +6,7 @@ using Leap.Unity;
 
 public class Destroy : MonoBehaviour
 {
+    public Transform explosionPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +14,22 @@ public class Destroy : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision collision) {
+
+      ContactPoint contact = collision.contacts[0];
+       // Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+       Vector3 pos = contact.point;
+       // Instantiate(explosionPrefab, pos, rot);
+        Instantiate (explosionPrefab, pos, Quaternion.identity);
+        Destroy(this.gameObject);
     HandModelBase hand = collision.gameObject.GetComponentInParent<HandModelBase>();
     if(hand != null){
+
+       // Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+       
+       // Instantiate(explosionPrefab, pos, rot);
+        Instantiate (explosionPrefab, pos, Quaternion.identity);
         Destroy(this.gameObject);
     }
+
   }
 }
