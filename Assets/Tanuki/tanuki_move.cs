67b8a7398db2f4e4fdd7785bbc27950cdfaef7e1
*Qpd7kq6p4x <donnebit@gmail.com>
2019-02-21 09:14:42 +0900

    SFX Attempt 2/21/2019

--- Assets/Tanuki/tanuki_move.cs
+++ Assets/Tanuki/tanuki_move.cs
@@ -8,18 +8,37 @@ public class tanuki_move : MonoBehaviour
     public Transform target;
     public Vector3 position;
 
+    public AudioClip racoonSound;
+    private AudioSource source;
+
+    private Vector3 curPos;
+    private Vector3 lastPos;
+
     // Start is called before the first frame update
     void Awake()
     {
         position = gameObject.transform.position;
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float step = speed * Time.deltaTime;
+        float step = speed * Time.deltaTime;    
 
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        curPos = position;
+        Debug.Log("Test");
+        
+        if(curPos != lastPos) {
+            Debug.Log("Test2");
+            if(!source.isPlaying){
+                source.clip = racoonSound;
+                source.Play();
+            }
+            
+        }
+        lastPos = curPos;
     }
 }
